706a8994d8c45051b3019c970cf79761556fddac
Dalton67 <dalton67665048@yahoo.com>
2020-09-19 14:20:11 -0500

    implementation of Members controller with CRUD funtionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'members#index'
+
+  resources :members do
+    member do
+      get :delete
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
