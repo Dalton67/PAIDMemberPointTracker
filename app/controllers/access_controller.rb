2a13c59eaaee19c7ba56f2b7a89cb03fd5d43cd1
kade182 <kadeflitton@gmail.com>
2020-09-22 21:26:55 -0400

    skeleton for login

---
+++ app/controllers/access_controller.rb
@@ -0,0 +1,18 @@
+class AccessController < ApplicationController
+  
+  layout 'admin'
+  
+  def menu
+    #display text and links
+  end
+
+  def login
+    # login form
+  end
+
+  def attempt_login
+  end
+
+  def attempt_logout
+  end
+end
