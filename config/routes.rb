1f92598f3caa86b259ed850bd34e3ae7c48d058d
kade182 <kadeflitton@gmail.com>
2020-10-09 15:03:03 -0400

    fixing route

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,10 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :admin_users, :except => [:show] do
+    member do
+      get :delete
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
