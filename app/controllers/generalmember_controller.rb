654908264bbf864ac0be8f6c8cead65b75972f61
Marco Ravelo <mravelo5874@tamu.edu>
2020-11-02 14:30:36 -0600

    added search / sort to general members page

--- app/controllers/generalmember_controller.rb
+++ app/controllers/generalmember_controller.rb
@@ -1,8 +1,39 @@
 # frozen_string_literal: true
 
 class GeneralmemberController < ApplicationController
+
   def index
-    @members = Member.order(:id)
+    @searched_members = Member.all
+    @members = Member.all
+
+    if params[:search] != ''
+      @searched_members = Member.search(params[:search]) if params[:search]
+      # puts "search"
+      puts params[:search]
+    else
+      @searched_members = Member.all
+      # puts "non-search"
+    end
+
+    if params[:sort] == 'total_points'
+      @members = @searched_members.all.order('total_points').reverse_order
+      # puts "total_points"
+    elsif params[:sort] == 'fall_points'
+      @members = @searched_members.all.order('fall_points').reverse_order
+      # puts "fall_points"
+    elsif params[:sort] == 'spring_points'
+      @members = @searched_members.all.order('spring_points').reverse_order
+      # puts "spring_points"
+    elsif params[:sort] == 'first_name'
+      @members = @searched_members.all.order('first_name')
+      # puts "first_name"
+    elsif params[:sort] == 'last_name'
+      @members = @searched_members.all.order('last_name')
+      # puts "last_name"
+    else
+      @members = @searched_members.all.order('total_points').reverse_order
+      # puts "default (total_points)"
+    end
   end
 
   private
