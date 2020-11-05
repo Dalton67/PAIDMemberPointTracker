08750c62a7acb7c7fd5839189dad1770102ac233
David <dchapa999@gmail.com>
2020-11-04 18:53:24 -0600

    Adding seperate pages for large tables and adding events edit page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,53 +1,114 @@
+# frozen_string_literal: true
+
 class EventsController < ApplicationController
-    
   before_action :confirm_logged_in
-    def index
-        @events = Event.order(:id)
-      end
+
+  def index
+    @events = Event.order(:id)
+
+    @events = Event.paginate(:page => params[:page], :per_page => 50)
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    @member = Member.new
+  end
+
+  def new
+    @events = Event.new
+  end
+
+  def create
+    @events = Event.new(event_params)
+    if @events.save
+      redirect_to(events_path)
+    else
+      render('new')
+#Previously in master 
+#     def index
+#         @events = Event.order(:id)
+#       end
     
-      def show
-        @event = Event.find(params[:id])
-        @member = Member.new
-      end
+#       def show
+#         @event = Event.find(params[:id])
+#         @member = Member.new
+#       end
     
-      def new
-        @events = Event.new
-      end
+#       def new
+#         @events = Event.new
+#       end
     
-      def create
-        @events = Event.new(event_params)
-        if @events.save
-          redirect_to(events_path)
-        else
-          render('new')
-        end
-      end
+#       def create
+#         @events = Event.new(event_params)
+#         if @events.save
+#           redirect_to(events_path)
+#         else
+#           render('new')
+#         end
+#       end
     
-      def edit
-        @events = Event.find(params[:id])
-      end
+#       def edit
+#         @event = Event.find(params[:id])
+#       end
     
-      def update
-        @events = Event.find(params[:id])
-        if @events.update_attributes(event_params)
-          redirect_to(event_path(@events))
-        else
-          render('new')
-        end
-      end
+#       def update
+#         @event = Event.find(params[:id])
+#         if @event.update_attributes(event_params)
+#           redirect_to(event_path(@event))
+#         else
+#           render('new')
+#         end
+#       end
     
-      def delete
-        @events = Event.find(params[:id])
-      end
+#       def delete
+#         @events = Event.find(params[:id])
+#       end
     
-      def destroy
-        @events = Event.find(params[:id])
-        @events.destroy
-        redirect_to(events_path)
-      end
+#       def destroy
+#         @events = Event.find(params[:id])
+#         @events.destroy
+#         redirect_to(events_path)
+#       end
     
-      private
-      def event_params
-        params.require(:event).permit(:id, :title, :date, :semester, :points_worth)
-      end
+#       private
+#       def event_params
+#         params.require(:event).permit(:id, :title, :date, :semester, :points_worth)
+#       end
+
     end
+  end
+
+  def edit
+    @events = Event.find(params[:id])
+  end
+
+  def update
+    @events = Event.find(params[:id])
+    if @events.update_attributes(event_params)
+      redirect_to(event_path(@events))
+    else
+      render('new')
+    end
+  end
+
+  def delete
+    @events = Event.find(params[:id])
+  end
+
+  def destroy
+    @events = Event.find(params[:id])
+    @events.destroy
+    redirect_to(events_path)
+  end
+
+  def getEvents
+    Event.apiEvents()
+    redirect_to(events_path)
+  end 
+
+  private
+
+  def event_params
+    params.require(:event).permit(:id, :title, :date, :semester, :points_worth)
+  end
+end
--- app/controllers/generalmember_controller.rb
+++ app/controllers/generalmember_controller.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 class GeneralmemberController < ApplicationController
-  
   def index
     @members = Member.order(:id)
+
+    @members = Member.paginate(:page => params[:page], :per_page => 50)
   end
 
   private
@@ -9,6 +12,4 @@ class GeneralmemberController < ApplicationController
   def member_params
     params.require(:member).permit(:id, :first_name, :last_name, :email, :fall_points, :spring_points, :total_points)
   end
-  
-
 end
